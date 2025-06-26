acb218311bd823ac5e052fa9fc8abb4d5c5d3909
sushannao <sushanna5858@gmail.com>
2025-06-26 11:25:50 -0400

    fullstackpro commit

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.fullstackproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.fullstackproject to javafx.fxml;
-    exports com.example.fullstackproject;
-}
